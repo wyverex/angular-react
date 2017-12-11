cebfc9db35dc0b673c4b18e09bfb2efca6bb2e08
Shirtless Kirk <dev+github@shirtlesskirk.com>
2017-12-11 13:32:06 +0000

    Extract and de-anonymise reducer functions

--- public/js/main.js
+++ public/js/main.js
@@ -8,6 +8,21 @@ var CHARTCONTROLLER = 'DoughnutController';
 var PAGECONTROLLER = 'PageController';
 var REMOTEURL = 'http://ergast.com/api/f1/current/driverStandings.json?limit=10';
 
+function labels(data) {
+    return data.map(function (item) {
+        var driver = item.Driver;
+
+        return driver.givenName + ' ' + driver.familyName +
+            ' (' + item.Constructors[0].name + ')';
+    });
+}
+
+function points(data) {
+    return data.map(function (item) {
+        return item.points;
+    });
+}
+
 app.config(['$routeProvider', function ($routeProvider) {
     $routeProvider
         .when('/', { templateUrl: 'partials/angular-chart.html', controller: CHARTCONTROLLER })
@@ -22,17 +37,8 @@ app.controller(CHARTCONTROLLER, function ($scope, $http) {
     function parseResponse (response) {
         var data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 
-        $scope.labels = data.map(function (item) {
-            var driver = item.Driver;
-
-            return driver.givenName + ' ' + driver.familyName +
-                ' (' + item.Constructors[0].name + ')';
-        });
-
-        $scope.data = data.map(function (item) {
-            return item.points;
-        });
-
+        $scope.labels = labels(data);
+        $scope.data = points(data);
         $scope.options = {
             legend: {
                 display: true,
--- src/angular/chart-controller.js
+++ src/angular/chart-controller.js
@@ -1,20 +1,26 @@
 var REMOTEURL = 'http://ergast.com/api/f1/current/driverStandings.json?limit=10';
 
+function labels(data) {
+    return data.map(function (item) {
+        var driver = item.Driver;
+
+        return driver.givenName + ' ' + driver.familyName +
+            ' (' + item.Constructors[0].name + ')';
+    });
+}
+
+function points(data) {
+    return data.map(function (item) {
+        return item.points;
+    });
+}
+
 function ChartController($scope, $http) {
     function parseResponse(response) {
         var data = response.data.MRData.StandingsTable.StandingsLists[0].DriverStandings;
 
-        $scope.labels = data.map(function (item) {
-            var driver = item.Driver;
-
-            return driver.givenName + ' ' + driver.familyName +
-                ' (' + item.Constructors[0].name + ')';
-        });
-
-        $scope.data = data.map(function (item) {
-            return item.points;
-        });
-
+        $scope.labels = labels(data);
+        $scope.data = points(data);
         $scope.options = {
             legend: {
                 display: true,
